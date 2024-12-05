b57160e2133a611eba1b10fad6955e51bd307e0e
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2024-12-05 16:58:34 +0200

    Adds new tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('il componente Welcome viene montato correttamente', () => {
+  // 1) monto il componente nel Virtual Dom
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  // 2) ricerco l'elemento (in questo caso il messaggio di benvenuto)
+  const welcome = screen.getByText(/Benvenuti in EpiBooks!/i)
+  // 3) non intendiamo al momento interagirci, skip!
+  // 4) verifica ipotesi/tesi
+  expect(welcome).toBeInTheDocument()
+})
+
+test('verifico quante card di libri vengono generate', () => {
+  // 1) monto il componente nel Virtual Dom
+  render(<App />)
+  // 2) ricerco gli elementi (in questo caso le card dei libri)
+  const cards = screen.getAllByTestId('book')
+  // 3) non intendiamo al momento interagirci, skip!
+  // 4) verifica ipotesi/tesi
+  expect(cards).toHaveLength(150)
 })
